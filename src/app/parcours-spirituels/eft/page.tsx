00b678e0e49b4c3c7702fddd4e0bd88548f31767
Louis Olivier <72631705+cyrano21@users.noreply.github.com>
2025-03-25 20:12:39 +0100

    add EFT

--- src/app/parcours-spirituels/eft/page.tsx
+++ src/app/parcours-spirituels/eft/page.tsx
@@ -1,21 +1,17 @@
 "use client";
 import Link from "next/link";
 import needsData from "@/data/needs.json";
+import { Need } from "@/types/need";
+
+// Accéder correctement au tableau needs dans le fichier JSON
+const allNeeds = (needsData as unknown as { needs: Need[] }).needs;
 
 export default function TapHome() {
-  const needs = needsData;
-
-
-  type Need = {
-    id: string;
-    title: string;
-  };
-
   return (
     <div className="p-6">
       <h1 className="text-3xl font-bold mb-4">Choisis ce que tu veux guérir</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {needs.map((need: Need) => (
+        {allNeeds.map((need) => (
           <Link
             key={need.id}
             href={`/parcours-spirituels/eft/${need.id}`}
